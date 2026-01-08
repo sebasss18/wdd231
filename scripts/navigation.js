b5b0633c93d6ffc8083240819ef8c11e326cd236
sebasss18 <sebasbernals11@gmail.com>
2026-01-07 18:38:15 -0700

    assignment

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector('#ham-button');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
