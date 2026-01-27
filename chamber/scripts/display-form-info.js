8ee79f33073c1db429f4c810dfccbd442af910da
sebasss18 <sebasbernals11@gmail.com>
2026-01-27 08:47:27 -0700

    fix form js

--- chamber/scripts/display-form-info.js
+++ chamber/scripts/display-form-info.js
@@ -2,10 +2,8 @@ const myInfo = new URLSearchParams(window.location.search);
 
 document.querySelector('#results').innerHTML = `
     <p><strong>Name:</strong> ${myInfo.get('first')} ${myInfo.get('last')}</p>
-    <p><strong>Organizational Title:</strong> ${myInfo.get('organization-title')}</p>
     <p><strong>Phone:</strong> ${myInfo.get('phone')}</p>
     <p><strong>Email:</strong> ${myInfo.get('email')}</p>
-    <p><strong>Business:</strong> ${myInfo.get('organization')}</p>
-    <p><strong>Membership Level:</strong> ${myInfo.get('membership')}</p>
-    <p><strong>Description:</strong> ${myInfo.get('description')}</p>
+    <p><strong>Business Name:</strong> ${myInfo.get('organization')}</p>
+    <p><strong>Date Submitted:</strong> ${myInfo.get('timestamp')}</p>
 `;
