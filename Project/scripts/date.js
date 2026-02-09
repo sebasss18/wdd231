9c9414510411f7043d57dcdb5db48560a0cdf95d
sebasss18 <sebasbernals11@gmail.com>
2026-02-09 15:52:03 -0700

    adding images

---
+++ Project/scripts/date.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
